04704f0f3962cea3cd1e82fc83aac2ce317ba17f
WuS1 <WuS1@ON34C03219184.mshome.net>
2022-04-02 17:37:21 -0400

    v6. devfile generated

--- gs-spring-boot-kubernetes-complete/src/main/java/com/example/demo/DemoApplication.java
+++ gs-spring-boot-kubernetes-complete/src/main/java/com/example/demo/DemoApplication.java
@@ -2,20 +2,21 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+
 @RestController
+@SpringBootApplication
 public class DemoApplication {
 
-	@GetMapping("/")
-	public String home() {
-		return "Hello";
-	}
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
